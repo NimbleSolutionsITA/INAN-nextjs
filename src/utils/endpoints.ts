195e6019b6043ca865971bca4a1e014a6423ef60
Maxime Di Chiara <maxime.dichiara@spindox.it>
2023-12-11 17:50:31 +0100

    fix enpoints

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,10 +1,10 @@
 export const WORDPRESS_API_ENDPOINT = `${ process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL}/wp-json`
 export const HEADER_FOOTER_ENDPOINT = `${ WORDPRESS_API_ENDPOINT}/rae/v1/header-footer?header_location_id=hcms-menu-header&footer_location_id=hcms-menu-footer`
 export const NEWS_FEED_ENDPOINT = `${ WORDPRESS_API_ENDPOINT}/wp/v2/news_feed`
-export const API_GET_PRODUCTS_ENDPOINT = `${ process.env.NEXT_PUBLIC_SITE_URL}/api/products`
-export const API_CUSTOMER_ENDPOINT = `${ process.env.NEXT_PUBLIC_SITE_URL}/api/customer`
-export const API_ORDER_ENDPOINT = `${ process.env.NEXT_PUBLIC_SITE_URL}/api/orders`
-export const API_SALES_ENDPOINT = `${ process.env.NEXT_PUBLIC_SITE_URL}/api/private-sales`
+export const API_GET_PRODUCTS_ENDPOINT = `/api/products`
+export const API_CUSTOMER_ENDPOINT = `/api/customer`
+export const API_ORDER_ENDPOINT = `/api/orders`
+export const API_SALES_ENDPOINT = `/api/private-sales`
 export const WORDPRESS_USER_INFO_ENDPOINT = `${WORDPRESS_API_ENDPOINT}/wp/v2/users/me`
 export const WORDPRESS_LOGIN_ENDPOINT = `${WORDPRESS_API_ENDPOINT}/jwt-auth/v1/token`
 export const WORDPRESS_RESET_PASSWORD_ENDPOINT = `${WORDPRESS_API_ENDPOINT}/bdpwr/v1/reset-password`
